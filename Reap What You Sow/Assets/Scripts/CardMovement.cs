c3dc91da25ce438666c82c2f1e70cebc1354d207
Grievousfan82 <turtlesarecool1021@gmail.com>
2025-10-15 01:41:18 -0500

    added the ability to drag a card out and let it snap back
    At this point it still needs trouble shooting but it is 2:00 AM so will pick up in morning

---
+++ Reap What You Sow/Assets/Scripts/CardMovement.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private Vector2 originalLocalPointerPosition;
+    private Vector3 originalPanelLocalPosition;
+    private Vector3 originalScale;
+    private int currentState = 0;
+    private Quaternion originalRotation;
+    private Vector3 originalPosition;
+    private InputAction mousePointer;
+
+    [SerializeField] private float selectScale = 1.1f;
+    [SerializeField] private Vector2 cardPlay;
+    [SerializeField] private Vector3 playPosition;
+    [SerializeField] private GameObject glowEffect;
+    [SerializeField] private GameObject playArrow;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        originalScale = rectTransform.localScale;
+        originalPosition = rectTransform.localPosition;
+        originalRotation = rectTransform.localRotation;
+        mousePointer = InputSystem.actions.FindAction("Point");
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case 1:
+                HandleHoverState();
+                break;
+            case 2:
+                HandleDragState();
+                if (!Mouse.current.leftButton.IsPressed())
+                {
+                    TransitionToState0();
+                }
+                break;
+            case 3:
+                HandlePlayState();
+                if (!Mouse.current.leftButton.IsPressed())
+                {
+                    TransitionToState0();
+                }
+                break;
+        }
+    }
+
+    private void TransitionToState0()
+    {
+        currentState = 0;
+        rectTransform.localScale = originalScale;
+        rectTransform.localRotation = originalRotation;
+        rectTransform.localPosition = originalPosition; ;
+        glowEffect.SetActive(false);
+        playArrow.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentState == 0)
+        {
+            originalPosition = rectTransform.localPosition;
+            originalRotation = rectTransform.localRotation;
+            originalScale = rectTransform.localScale;
+
+            currentState = 1;
+        }
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (currentState == 1)
+        {
+            TransitionToState0();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (currentState == 1)
+        {
+            currentState = 2;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
+            originalPanelLocalPosition = rectTransform.localPosition;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (currentState == 2)
+        {
+            Vector2 localPointerPosition;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPointerPosition))
+            {
+                rectTransform.position = mousePointer.ReadValue<Vector2>();
+
+                if (rectTransform.localPosition.y > cardPlay.y)
+                {
+                    currentState = 3;
+                    playArrow.SetActive(true);
+                    rectTransform.localPosition = playPosition;
+                }
+            }
+        }
+    }
+    private void HandleHoverState()
+    {
+        glowEffect.SetActive(true);
+        rectTransform.localScale = originalScale * selectScale;
+    }
+
+    private void HandleDragState()
+    {
+        rectTransform.localRotation = Quaternion.identity;
+    }
+
+    private void HandlePlayState()
+    {
+        rectTransform.localPosition = playPosition;
+        rectTransform.localRotation = Quaternion.identity;
+
+        if (mousePointer.ReadValue<Vector2>().y < cardPlay.y)
+        {
+            currentState = 2;
+            playArrow.SetActive(false);
+        }
+    }
+}
